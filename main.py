24a975abe9f07d032305f87463fe07325a2736dc
JiaWeiWong7 <67058800+JiaWeiWong7@users.noreply.github.com>
2023-02-03 19:34:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,18 @@ def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
   return delta.days
 
+def get_countB():
+  delta = today - datetime.strptime("2021-11-5", "%Y-%m-%d")
+  return delta.days
+
 def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  if next < datetime.now():
+    next = next.replace(year=next.year + 1)
+  return (next - today).days
+
+def get_birthdayB():
+  next = datetime.strptime(str(date.today().year) + "-" + "10-03", "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
@@ -45,9 +55,16 @@ def get_random_color():
 
 
 client = WeChatClient(app_id, app_secret)
+client2 = WeChatClient("wx43670927a37a0863", "4fcef27194be995c9cf7de76531f732a")
 
 wm = WeChatMessage(client)
+wm2 = WeChatMessage(client2)
+
 wea, temperature = get_weather()
 data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+data2 = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_countB()},"birthday_left":{"value":get_birthdayB()},"words":{"value":get_words(), "color":get_random_color()}}
+
 res = wm.send_template(user_id, template_id, data)
+res2 = wm2.send_template("okf1F6Mx5dcSMTDgGoYtMl81UE9Y", "Zg_p9Q6_ZsAWaThtMh_OLP-GmQ9iiGeNlYuLPSMgqBg", data2)
 print(res)
+print(res2)
